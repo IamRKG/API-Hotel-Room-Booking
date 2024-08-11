fb144a0ca9d6a305ac19355011eb0981ad859198
IamRKG <radhacreative@gmail.com>
2024-08-11 14:30:14 +0530

    add atlas (#3)

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+//const MONGODB_URI = `mongodb+srv://user_admin:qwerty123456@hotel-room-booking.jh4uw.mongodb.net/?retryWrites=true&w=majority&appName=hotel-room-booking`
+
+const MONGODB_URI = `mongodb+srv://user_admin:${encodeURIComponent('qwerty123456')}@hotel-room-booking.jh4uw.mongodb.net/hotel_booking?retryWrites=true&w=majority`;
 
 const connectDB = async (): Promise<void> => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/hotel_booking');
+    console.log('Connecting to MongoDB...', MONGODB_URI);
+    const conn = await mongoose.connect(MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error}`);
@@ -11,3 +19,4 @@ const connectDB = async (): Promise<void> => {
 };
 
 export default connectDB;
+
